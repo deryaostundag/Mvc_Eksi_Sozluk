2c37ea913d5d07efb9e76a01650fa4768b821d8d
Hp <Hp@DESKTOP-BUOAOVG>
2023-04-03 23:34:54 +0300

    25. Derse kadar olan değişiklikler

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,31 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+        public List<Category> GetAllBL()
+        {
+            return repo.List();
+        }
+        public void CategoryAddBL(Category category)
+        {
+            //if (category.CategoryName != null || category.CategoryName.Length <= 3 || category.CategoryDescription == "" || category.CategoryName.Length >= 51)
+            //{
+            //    //HATA MESAJI
+            //}
+            //else
+            //{
+                repo.Insert(category);
+            //}
+        }
+
+    }
+}
---
+++ MvcProjeKampi/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        CategoryManager cm=new CategoryManager();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetCategoryList()
+        {
+            var categoryvalues = cm.GetAllBL();
+            return View(categoryvalues);
+        }
+        public ActionResult AddCategory(Category category)
+        {
+            cm.CategoryAddBL(category);
+            return RedirectToAction("GetCategoryList");
+        }
+
+    }
+}
