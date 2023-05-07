acb76bcf89d4aa7dcf4053c13f7be563bfc1c8bd
Hp <Hp@DESKTOP-BUOAOVG>
2023-05-07 22:06:35 +0300

    Ders35

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
 using DataAccessLayer.Concrete.Repositories;
 using EntityLayer.Concrete;
 using System;
@@ -11,25 +12,21 @@ namespace BusinessLayer.Concrete
 {
     public class CategoryManager : ICategoryService
     {
-        //GenericRepository<Category> repo = new GenericRepository<Category>();
-        //public List<Category> GetAllBL()
-        //{
-        //    return repo.List();
-        //}
-        //public void CategoryAddBL(Category category)
-        //{
-        //    if (category.CategoryName != null || category.CategoryName.Length <= 3 || category.CategoryDescription == "" || category.CategoryName.Length >= 51)
-        //    {
-        //        //HATA MESAJI
-        //    }
-        //    else
-        //    {
-        //        repo.Insert(category);
-        //    }
-        //}
+        ICategoryDal _ICategorydal;
+        public CategoryManager(ICategoryDal ıCategorydal)
+        {
+            _ICategorydal = ıCategorydal;
+        }
+
+        public void CategoryAdd(Category category)
+        {
+            _ICategorydal.Insert(category);
+        }
+
         public List<Category> GetList()
         {
-            
+            return _ICategorydal.List();
         }
+        
     }
 }
--- MvcProjeKampi/Controllers/CategoryController.cs
+++ MvcProjeKampi/Controllers/CategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules_FluentValidation;
+using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,26 +14,39 @@ namespace MvcProjeKampi.Controllers
     public class CategoryController : Controller
     {
         // GET: Category
-        CategoryManager cm=new CategoryManager();
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
         public ActionResult Index()
         {
             return View();
         }
         public ActionResult GetCategoryList()
         {
-            //var categoryvalues = cm.GetAllBL();
-            return View();
+            var categoryvalues = cm.GetList();
+            return View(categoryvalues);
         }
-        [HttpGet] 
-        public ActionResult AddCategory() 
+        [HttpGet]
+        public ActionResult AddCategory()
         {
             return View();
         }
         [HttpPost]
         public ActionResult AddCategory(Category category)
         {
-           // cm.CategoryAddBL(category);
-            return RedirectToAction("GetCategoryList");
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult result = categoryValidator.Validate(category);
+            if (result.IsValid)
+            {
+                cm.CategoryAdd(category);
+                return RedirectToAction("GetCategoryList");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
 
     }
