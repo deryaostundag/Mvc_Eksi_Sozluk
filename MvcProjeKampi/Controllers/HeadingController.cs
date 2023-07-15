a0ad86a610b30a39b15122d6968fad2252c2dd9e
Hp <Hp@DESKTOP-BUOAOVG>
2023-07-15 16:11:16 +0300

    Ders 52

--- MvcProjeKampi/Controllers/HeadingController.cs
+++ MvcProjeKampi/Controllers/HeadingController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +13,44 @@ namespace MvcProjeKampi.Controllers
     {
         // GET: Heading
         HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        WriterManager wm=new WriterManager(new EfWriterDal());
         public ActionResult Index()
         {
             var headingValues=hm.GetList();
             return View(headingValues);
+        }
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> valueCategory=(from x in cm.GetList()
+                                                select new SelectListItem
+                                                {
+                                                    Text=x.CategoryName, 
+                                                    Value=x.CategoryID.ToString(),
+                                                }).ToList();
+            ViewBag.vlc=valueCategory;
+
+            List<SelectListItem> valueWriter = (from x in wm.GetList()
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.WriterName +" "+ x.WriterSurname,
+                                                    Value = x.WriterID.ToString(),
+                                                }).ToList();
+            ViewBag.vlw=valueWriter;
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddHeading(Heading h)
+        {
+            h.HeadingDate= DateTime.Parse(DateTime.Now.ToShortDateString());
+            hm.HeadingAdd(h);
+            return RedirectToAction("Index");
+        }
+        public ActionResult ContentByHeading() 
+        { 
+            return View(); 
         }
     }
 }
